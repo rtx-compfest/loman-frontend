2a28772f3aeaf71ef407f57a7b3f3e04bda74300
abdmmar <opowae909@gmail.com>
2021-08-13 23:38:56 +0700

    Add basic form and input using Formik and Chakra

--- pages/fundraiser/create.js
+++ pages/fundraiser/create.js
@@ -1,10 +1,25 @@
 import Head from 'next/head'
-import {Container, Grid, Heading} from '@chakra-ui/react'
+import * as Yup from 'yup'
+import {Formik, Field, Form} from 'formik'
+import {
+  Button,
+  Container,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Grid,
+  Heading,
+  Input,
+} from '@chakra-ui/react'
 
 import {NavFundraiser} from '@components/Nav'
 import Footer from '@components/Footer'
 
 export default function Create() {
+  const formSubmit = (values) => {
+    console.log(values)
+  }
+
   return (
     <Container maxWidth="container.lg">
       <Head>
@@ -25,7 +40,48 @@ export default function Create() {
           borderWidth="1px"
           borderRadius="6"
         >
-          <Heading size="xl">New Fundraising</Heading>
+          <Heading size="xl" mb="5">
+            New Fundraising
+          </Heading>
+          <Formik
+            initialValues={{fundraisingName: ''}}
+            validationSchema={Yup.object({
+              fundraisingName: Yup.string().required(
+                'Fundraising Name is Required',
+              ),
+            })}
+            onSubmit={formSubmit}
+          >
+            <Form>
+              <Field name="fundraisingName" type="text">
+                {({field, form}) => (
+                  <FormControl
+                    marginBlock="4"
+                    isInvalid={
+                      form.errors.fundraisingName &&
+                      form.touched.fundraisingName
+                    }
+                  >
+                    <FormLabel htmlFor="fundraisingName">
+                      Fundraising Name
+                    </FormLabel>
+                    <Input {...field} id="fundraisingName" />
+                    <FormErrorMessage>
+                      {form.errors.fundraisingName}
+                    </FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Button
+                type="submit"
+                variant="solid"
+                colorScheme="green"
+                width="100%"
+              >
+                Create New Fundraising
+              </Button>
+            </Form>
+          </Formik>
         </Grid>
       </Grid>
 
