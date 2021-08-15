bd99478dd9497a05b503b0b819f0a9004c7191e7
abdmmar <opowae909@gmail.com>
2021-08-15 14:52:07 +0700

    Add react-query

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
 import '../styles/globals.css'
-import {ChakraProvider} from "@chakra-ui/react"
+import {ChakraProvider} from '@chakra-ui/react'
+import {QueryClient, QueryClientProvider} from 'react-query'
+import {Hydrate} from 'react-query/hydration'
 
 function MyApp({Component, pageProps}) {
-    return (
+  const [queryClient] = React.useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
         <ChakraProvider>
-            <Component {...pageProps} />
+          <Component {...pageProps} />
         </ChakraProvider>
-    )
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
